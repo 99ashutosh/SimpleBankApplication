dd2b72282eaa68af1d03a538eaaba39dc76b838a
Ashutosh <73209665+99ashutosh@users.noreply.github.com>
2021-08-19 17:32:24 +0530

    Delete oldsrc directory

--- oldsrc/bank.h
+++
@@ -1,7 +0,0 @@
-// csv_db functions
-int init();
-int find(char *username);
-int login(char *username, char *password);
-int create_new_account(char *username, char *password, int number, char *email);
-int flush();
-void gen_upi_handler(char handler[], int passcode);
